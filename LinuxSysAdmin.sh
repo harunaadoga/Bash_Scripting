25f34fb5c013bdd68448d152fbbe56f1af608124
harunaadoga <haruna.umar.adoga@gmail.com>
2023-04-08 18:46:02 +0100

    Added Menu Option

--- LinuxSysAdmin.sh
+++ LinuxSysAdmin.sh
@@ -1,6 +1,6 @@
-X#! /bin/bash
-echo 'Welcome to Hartechworld Linux System Admin Dashboard!!!'
-CHOICE="Your choice:"
+#! /bin/bash
+echo 'Welcome to Hartechworld Linux System Admin Menu!!!'
+PS3="Your choice:"
 select ITEM in "Add user" "List All Processes" "Kill Process" "Install Program" "Quit"
 do
 if [[ $REPLY -eq 1 ]]
@@ -12,5 +12,38 @@ then
                 echo "The username $username already exists."
         else
                 sudo usersadd -m -s /bin/bash "$username"
+		if [[ $? -eq 0 ]]
+		then 
+			echo "The user $username was added successfully."
+			tail -n 1 /etc/passwd		
+		else
+			echo "There was an error adding the user $username."
+		fi
+	fi
+
+elif [[ $REPLY -eq 2 ]]
+then
+	echo "Listing all processes..."
+	sleep 1
+	ps -ef
+
+elif [[ $REPLY -eq 3 ]]
+then
+	read -p "Enter the process to kill:" process
+	pkill $process
+
+elif [[ $REPLY -eq 4 ]]
+then
+	read -p "Enter the program to install:" app
+	sudo apt update && sudo sudo apt install $app
+
+elif [[ $REPLY -eq 5 ]]
+then
+	echo "Quitting..."
+	sleep 1
+	exit
+else
+	echo "Invalid menu selection"
+fi
 
 done
